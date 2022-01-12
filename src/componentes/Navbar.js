3ec39f54dda4c94b0722692635799e717d54a50c
MarcoCairus <marcairus97@gmail.com>
2022-01-11 22:11:29 -0300

    Cambios Proyecto

---
+++ src/componentes/Navbar.js
@@ -0,0 +1,25 @@
+
+const navBar = () => {
+    return (
+        
+        <nav>
+            <h1>Una Delicia</h1>
+            <ul>
+                <li>
+                <a href='/#'>Inicio</a>
+                </li>
+                <li>
+                <a href='/#'>Menu</a>
+                </li>
+                <li>
+                <a href='/#'>Informacion</a>
+                </li>
+            </ul>
+            <a href='/#'>Login</a>
+        </nav>
+        
+        
+    )
+}
+
+export default navBar
